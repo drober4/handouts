4261d7caa21ba3758b3a284ac30ee97f2762015a
Dustin Robertson <drober4@tulane.edu>
2018-07-25 09:57:10 -0400

    Config

--- worksheet.sh
+++ worksheet.sh
@@ -1,10 +1,11 @@
 # configure git
 
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name "Dustin Robertson""
+git config --global user.email "drober4@tulane.edu"
 git commit --no-edit --amend --reset-author
 
 # link your local repository to the origin repository on GitHub
 
-git remote add origin ...
-git push -u origin master
+#git remote add origin ...
+#git push -u origin master
+
